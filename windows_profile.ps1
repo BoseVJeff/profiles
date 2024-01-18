7ad12d3cabbb69871b205ad1dc5d2451400465e5
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-01-18 20:03:48 +0530

    Prettifying `windows_profile.ps1`.

--- windows_profile.ps1
+++ windows_profile.ps1
@@ -7,11 +7,23 @@
 # Initialising with an empty path to prevent interference from host system paths
 $Env:Path = ""
 
-# Adding Powershell to PATH
+################################################################################
+
+# PowerShell
+
+# Adding PowerShell to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\PowerShell-7.4.0-win-x64"
+
+################################################################################
+
+# Git
 
 # Adding Git to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\git\bin"
+
+################################################################################
+
+# Flutter & Dart
 
 # Adding Flutter and Dart to PATH
 # Note that for the command to work, Git must be available on PATH and `git rev-parse HEAD` must not fail on the Flutter SDK repo.
@@ -24,19 +36,31 @@ git config --global --add safe.directory "$DriveRoot/windows/sdks/flutter_window
 # Taken from https://dart.dev/tools/pub/environment-variables
 $Env:PUB_CACHE = "$DriveRoot\windows\caches\pub"
 
-# Adding `where.exe` to PATH. Extracted from `C:\Windows\system32` on a Win10 install.
-# This allows `flutter doctor` and  command to work.
+# Adding `where.exe` and `attrib.exe` to PATH. Extracted from `C:\Windows\system32` on a Win10 install.
+# This allows `flutter doctor` and `flutter build windows` commands to work.
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sys32"
+
+################################################################################
+
+# Visual Studio Code
 
 # Adding VS Code to PATH
 # This allows `code .` to work in the terminal
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\VSCode\bin"
+
+################################################################################
+
+# Sublime Merge & Text
 
 # Adding Sublime Merge to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\sublime_merge_build_2091_x64"
 
 # Adding Sublime Text to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\sublime_text_build_4169_x64"
+
+################################################################################
+
+# Python
 
 # Adding Python to PATH
 # For this purpose, the (WinPython)[https://winpython.github.io/] distribtion is used
@@ -46,14 +70,24 @@ $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\winpython\WPy64-31160\python-3
 # This allows tools like `jupyter` and `flask` to work
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\winpython\WPy64-31160\python-3.11.6.amd64\Scripts"
 
+################################################################################
+
+# NodeJS and NPM
+
 # Adding NodeJS (inc npm) to PATH
 # This is reusing the NodeJS installed as a part of WinPython to save on space
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\winpython\WPy64-31160\n"
 
-#Setting up JAVA_HOME
+################################################################################
+
+# Android Studio, SDK and JAVA
+
 # For a ref on how the difference is reflected in `java --version` output, see https://stackoverflow.com/a/48336582
-# $Env:JAVA_HOME = "$DriveRoot\windows\sdks\open_jsk\jdk-21.0.1+12"  # This is OpenJDK. Keeping this as an option but disabled as Android STudio reccomends against it.
-$Env:JAVA_HOME = "$DriveRoot\windows\sdks\oracle_jdk\jdk-21.0.2"
+$OpenJDKHome = "$DriveRoot\windows\sdks\open_jsk\jdk-21.0.1+12"     # This is OpenJDK. Keeping this as an option but disabled as Android STudio reccomends against it.
+$OracleJDKHome = "$DriveRoot\windows\sdks\oracle_jdk\jdk-21.0.2"    # This is OracleJDK. Using this as reccomended by Android Studio (apparently for perf reasons).
+
+#Setting up JAVA_HOME
+$Env:JAVA_HOME = $OracleJDKHome
 
 # Adding Java to PATH
 # This is meant for Android development.
@@ -82,26 +116,54 @@ $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\android-studio\bin"
 $Env:STUDIO_JDK=$Env:JAVA_HOME	                                    # Sets the location of the JDK that Android Studio runs in. When you launch the IDE, it checks the STUDIO_JDK, JDK_HOME, and JAVA_HOME environment variables, in that order.
 $Env:STUDIO_GRADLE_JDK=$Env:JAVA_HOME	                            # Sets the location of the JDK that Android Studio uses to start the Gradle daemon. When you launch the IDE, it first checks STUDIO_GRADLE_JDK. If STUDIO_GRADLE_JDK is not defined, the IDE uses the value set in the project structure settings.
 
+################################################################################
+
+# .NET
+
 # Adding .NET to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\dotnet-sdk-8.0.100-win-x64"
+
+################################################################################
+
+# Just
 
 # Adding `just` to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\just"
 
+################################################################################
+
+# SQLite CLI
+
 # Adding SQLite CLI to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\sqlite-tools-win-x64-3450000"
 
+################################################################################
+
+# 7Zip CLI
+
 # Adding 7z CLI to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\7z2301-extra"
+
+################################################################################
+
+# C/C++/Fortran
 
 # Adding w64devkit to PATH
 # This allows for C(gcc)/C++(g++)/Fortran(gfortran) dev
 $Env:Path = $Env:Path + ";$DriveRoot\windows\sdks\w64devkit\bin"
 
+################################################################################
+
+# Vivaldi
+
 # Adding Vivaldi to PATH
 $Env:Path = $Env:Path + ";$DriveRoot\windows\apps\Vivaldi\Application"
 # Setting a seperate variable here so that Flutter uses Vivaldi for debugging purposes
 $Env:CHROME_EXECUTABLE="$DriveRoot\windows\apps\Vivaldi\Application\vivaldi.exe"
+
+################################################################################
+
+# Misc
 
 # Setting Local App Data folder
 $Env:LOCALAPPDATA = "$DriveRoot\windows\appdata\local"
@@ -109,11 +171,6 @@ $Env:LOCALAPPDATA = "$DriveRoot\windows\appdata\local"
 # Additional Flutter setup
 flutter config --android-sdk "$DriveRoot\windows\sdks\commandlinetools-win" | Out-Null
 flutter config --android-studio-dir "$DriveRoot\windows\apps\android-studio" | Out-Null
-
-################################################################################
-
-# Other Windows-specific settings
-
 
 # This is the windows equivalent of `$HOME` so only use this if all else fails
 # $Env:USERPROFILE="D:\windows\user"
